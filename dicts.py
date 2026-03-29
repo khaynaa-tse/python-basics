7e9c366dba1abd8626d24f9f65786b6dc716fb2d
unknown <khaynaa.tse@gmail.com>
2026-03-29 23:02:36 +0800

    Add list operations example

---
+++ dicts.py
@@ -0,0 +1,11 @@
+person = {
+    "name": "Johny",
+    "age": 36,
+    "city": "Shand"
+}
+
+print("name:", person["name"])
+print("age", person["age"])
+
+person["Job"] = "Engineer"
+print("Updated dictionary:", person)
