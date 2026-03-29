98d5140cd1f98b3be677f2b90e42307fdb03e151
unknown <khaynaa.tse@gmail.com>
2026-03-29 22:44:17 +0800

    Add list operations example

---
+++ lists.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print("First number:", numbers[6])
+
+numbers.append(50)
+print("Updated list:", numbers)
+
+for num in numbers:
+    print("Number:", num)
+
+    
