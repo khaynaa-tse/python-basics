d85f11cc6bd20dbed5f9f9c559064d7ffa398bfb
unknown <khaynaa.tse@gmail.com>
2026-03-30 23:46:05 +0800

    add nested dictionary example in dicts.py

--- calculator.py
+++ calculator.py
@@ -5,3 +5,20 @@ print("addition:", a+b)
 print("subtraction:", a-b)
 print("Multiplication:", a*b)
 print("Division:", a/b)
+
+x = 11
+x =x+7
+
+print (x)
+
+score = 0
+print("start:", score)
+
+score = score + 10
+print("after adding 10:", score)
+
+score = score - 3
+print("after subtracting 3:", score)
+
+score = score * 2
+print("after multiplieing two:", score)
--- dicts.py
+++ dicts.py
@@ -9,3 +9,36 @@ print("age", person["age"])
 
 person["Job"] = "Engineer"
 print("Updated dictionary:", person)
+
+names = ["Nara", "Mateve", "Boogii"]
+names.sort()
+print(names)
+print(names[0:2])
+print(names[1:3])
+
+student = {"nara": "sara", "grades": {"math": 90, "english": 85}}
+print(student["grades"]["english"])
+
+people = [
+    {"name": "sara", "age": 25},
+    {"name": "tse", "age": 30}
+]
+
+for person in people:
+    print(person["name"], "is", person["age"], "years old")
+
+person = {"name": "sara", "age": 28, "city": "darkhan"}
+print(person["name"])
+print(person["age"])
+print(person["city"])
+person["age"] = 41
+print(person["age"])
+person["job"] = "engineer"
+print(person["job"])
+
+student = {
+    "name": "Tse",
+    "grades": {"math": 91, "english": 89}
+}
+
+print(student["grades"]["math"])
